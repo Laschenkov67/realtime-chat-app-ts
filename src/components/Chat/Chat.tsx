f0666bf178e62273c7481f9bec80095919639c4d
laschenkov.artem <laschenkov.artem@otr.ru>
2020-02-17 22:58:39 +0300

    create chat and infobar component

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,9 +1,29 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
+import queryString from 'query-string';
+import io from "socket.io-client";
 
-const Chat: React.FC = () => {
+interface ChatProps {
+    location: any;
+}
 
+let socket;
+
+const Chat:React.FC<ChatProps> = ({ location }) => {
     const [name, setName] = useState<string>("");
     const [room, setRoom] = useState<string>("");
+    const ENDPOINT = 'localhost:5000';
+
+    useEffect(() => {
+        const {name: any, room: string} = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+
+        console.log(socket);
+
+    }, [ENDPOINT, location.search]);
 
     return (
         <div>
